3914895aa8c267a6a242fecd4b5edb33ff4644b4
mahsen099 <75514973+mahsen099@users.noreply.github.com>
2021-02-08 21:55:28 -0600

    new after

--- netflixclone/src/Row.js
+++ netflixclone/src/Row.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import  './Row.css';
 import axios from "./axios";
+
 function Row({title, fetchUrl}) {
+    const [movies, setMovies]= useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
     return (
         <div>
          <h1>{title}</h1>   
