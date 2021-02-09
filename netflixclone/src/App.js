75f2fc54f1f1b6c0789a220afc9361b68dab2808
mahsen099 <75514973+mahsen099@users.noreply.github.com>
2021-02-09 01:25:24 -0600

    newfile

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div>
    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
    <Row title="TRENDING Now" fetchUrl={requests.fetchTrending}/>
+   <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+   <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+   <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+   <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+   <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+   <Row title="Documentaries Movies" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflixclone/src/Row.js
+++ netflixclone/src/Row.js
@@ -1,22 +1,35 @@
-import React, { useState,useEffect } from 'react'
-import  './Row.css';
+import React, { useState, useEffect } from "react";
+import "./Row.css";
 import axios from "./axios";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies]= useState([]);
+const base_url = "http://image.tmdb.org/t/p/original/";
 
-    useEffect(()=>{
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
-    return (
-        <div>
-         <h1>{title}</h1>   
-        </div>
-    )
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // console.log(request)
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Row;
